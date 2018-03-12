1ae16719d828780be65814a2b47d3e6e3d8d561d
griooo <gri@bk.ru>
2018-03-12 21:36:46 +0300

    logger

--- src/main/java/gr/com/arounder/ArounderController.java
+++ src/main/java/gr/com/arounder/ArounderController.java
@@ -1,5 +1,7 @@
 package gr.com.arounder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,16 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArounderController implements ArounderCloud {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(ArounderController.class);
+
     @Value("${arounder.item:@}")
     private String item;
 
     @Override
     public String getItem() {
+        LOGGER.warn("Arounder GetItem ");
         return item;
     }
 
     @Override
     public String around(@PathVariable String value) {
+        LOGGER.warn("around for '{}'", value);
         return item + value + new StringBuilder(item).reverse().toString();
     }
 
