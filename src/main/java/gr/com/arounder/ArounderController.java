3ec40e6b7df30c7e7eae6484946fc1b1743a21b9
griooo <gri@bk.ru>
2018-03-12 22:24:36 +0300

    новые методы

--- src/main/java/gr/com/arounder/ArounderController.java
+++ src/main/java/gr/com/arounder/ArounderController.java
@@ -1,11 +1,14 @@
 package gr.com.arounder;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RefreshScope
 @RestController
@@ -26,6 +29,20 @@ public class ArounderController implements ArounderCloud {
     public String around(@PathVariable String value) {
         LOGGER.warn("around for '{}'", value);
         return item + value + new StringBuilder(item).reverse().toString();
+    }
+
+    @Override
+    public int hash(String value) {
+        LOGGER.warn("start hash calc for '{}'", value);
+        Uninterruptibles.sleepUninterruptibly(1000, TimeUnit.MILLISECONDS);
+        LOGGER.warn("hash calc for '{}' finish ", value);
+        return value.hashCode();
+    }
+
+    @Override
+    public int fuck() {
+        LOGGER.warn("catch my fuck!!!!!!");
+        throw new RuntimeException("Hello From Fucker");
     }
 
 
