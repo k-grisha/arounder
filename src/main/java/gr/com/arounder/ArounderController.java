c015bc549b400cfa13b89646cb1f121201a20050
griooo <gri@bk.ru>
2018-03-21 22:55:17 +0300

    logger

--- src/main/java/gr/com/arounder/ArounderController.java
+++ src/main/java/gr/com/arounder/ArounderController.java
@@ -1,14 +1,11 @@
 package gr.com.arounder;
 
-import com.google.common.util.concurrent.Uninterruptibles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 @RefreshScope
 @RestController
@@ -21,25 +18,25 @@ public class ArounderController implements ArounderCloud {
 
     @Override
     public String getItem() {
-        LOGGER.warn("Arounder GetItem ");
+        LOGGER.info("Arounder GetItem ");
         return item;
     }
 
     @Override
     public String around(@PathVariable String value) {
-        LOGGER.warn("around for '{}'", value);
+        LOGGER.info("around for '{}'", value);
         return item + value + new StringBuilder(item).reverse().toString();
     }
 
     @Override
-    public int hash(@PathVariable String value){
-        LOGGER.warn("start hash calc for '{}'", value);
+    public int hash(@PathVariable String value) {
+        LOGGER.info("start hash calc for '{}'", value);
         try {
-            Thread.sleep(1001);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        LOGGER.warn("finish hash calc for '{}'  ", value);
+        LOGGER.info("finish hash calc for '{}'  ", value);
         return value.hashCode();
     }
 
